10e9d222b57af5f0e90695ea07dfdc4960f2a177
AlexDanihevych <danifromalex@gmail.com>
2021-08-17 22:19:07 +0300

    created menu

---
+++ Main.java
@@ -0,0 +1,32 @@
+package java_core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception, IOException {
+		
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		System.out.println("Enter the number of task");
+		int taskNum = Integer.parseInt(br.readLine());
+		
+		// тут вставляємо main наших задач і будемо викликати за допомогою switch-case
+		// добавляємо нові case по нумерації 1,2,3, ....
+		
+		while (true) {
+			
+			switch (taskNum) {
+			
+			case 1:
+				
+				Task.main(args);
+			
+			}
+			
+		}
+		
+}
+}
+
