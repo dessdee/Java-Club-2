64b8b697cd46919225bfb66eb931c4b9c89d2ec3
IriOtto <iriottosan@gmail.com>
2021-08-18 20:50:07 +0300

    t6

---
+++ Task6.java
@@ -0,0 +1,9 @@
+package java_core;
+
+public class Task6 {
+	  public static int toBinary(int value) {
+		  int n = Integer.valueOf(Integer.toBinaryString(value));
+		      return n;
+		        }
+		     
+}
