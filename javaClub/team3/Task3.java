52c81532099d794ba89ff601db1cef324e00ce2e
Kuzbyt Maksym <max.dogmarion.lviv@gmail.com>
2021-08-18 22:15:17 +0300

    111

--- javaClub/team3/Task3.java
+++
@@ -1,7 +0,0 @@
-package javaClub.team3;
-
-public class Task3 {
-	  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
-		    return length * width * height;
-		  }
-}
