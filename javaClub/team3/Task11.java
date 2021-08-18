d20269dc0e87343e476e00b8349c59286e51d986
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-18 19:30:26 +0300

    Task 2, 7, 11

---
+++ javaClub/team3/Task11.java
@@ -0,0 +1,23 @@
+/*
+* Debug celsius converter
+* Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius.
+* Unfortunately his code has some bugs.
+* Find the errors in the code to get the celsius converter working properly.
+* https://www.codewars.com/kata/grasshopper-debug
+* */
+
+package javaClub.team3;
+
+public class Task11 {
+    public static String weatherInfo(int temp) {
+        double c = convertToCelsius(temp);
+        if (c <= 0)
+            return (c + " is freezing temperature");
+        else
+            return (c + " is above freezing temperature");
+    }
+
+    public static double convertToCelsius(int temperature) {
+        return (temperature - 32) * (5.0/9.0);
+    }
+}
